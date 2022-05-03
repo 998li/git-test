ca79e45412ec7bb0589d3d57bda8bfb5ac035f8b
998li <104730205+998li@users.noreply.github.com>
2022-05-03 20:34:05 +0800

    Update git.java

--- src/main/java/com.atguigu.git/Git/git.java
+++ src/main/java/com.atguigu.git/Git/git.java
@@ -9,6 +9,6 @@ public class git {
         System.out.println("hello,git4");
         System.out.println("master,test");
         System.out.println("hello,test");
-        System.out.println("push,test");
+        System.out.println("push,test1231");
     }
 }
